4f17767cda183e30d51641432362e5454033f39e
Mthokozisi Malaza <mthokozisi.malaza@absa.africa>
2023-09-15 06:52:19 +0200

    Added Constants class

---
+++ PokeMe/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  PokeMe
+//
+//  Created by Mthokozisi Malaza on 2023/09/14.
+//
+
+import UIKit
+
+class Constants {
+    static let url = "https://pokeapi.co/api/v2/pokemon/?limit=100"
+    static let spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/"
+}
